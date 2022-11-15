6f412c5aaf76e400ca54465870c6bd7ab941adb0
turtle85917 <siu85917@gmail.com>
2022-11-16 08:20:08 +0900

    Player move (x)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 const (
@@ -23,24 +24,89 @@ var (
 	brown      = color.RGBA{95, 85, 60, 255}
 	mint       = color.RGBA{0, 255, 166, 255}
 
-	player = Player{x: 3, y: 6}
+	player = Player{Point{x: 3, y: 6}}
+	block  = []Block{}
 
 	board = [height][width]int{}
 )
 
 type Game struct{}
 
-type Player struct {
+type Point struct {
 	x int
 	y int
 }
 
+type Player struct {
+	Point
+}
+
+type Block struct {
+	Point
+}
+
 func (g *Game) Update() error {
+	var directionX int
+	var directionY int
+	if inpututil.IsKeyJustPressed(ebiten.KeyLeft) {
+		directionX = -1
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+		directionX = 1
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+		directionY = -1
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
+		directionY = 1
+	}
+
+	for idx := 0; idx < width; idx++ {
+		if directionX != 0 {
+			tx := directionX * (idx + 1)
+			bf := blockFilter(block, func(b Block) bool {
+				return (b.x-directionX == player.x+tx && b.y == player.y)
+			})
+
+			if len(bf) > 0 || player.x+tx < 1 || player.x+tx > width-2 {
+				player.x += directionX * (idx + 1)
+				break
+			}
+		}
+	}
+
+	player.y += directionY
+
+	if player.x < 0 {
+		player.x = 0
+	}
+	if player.x > width-1 {
+		player.x = width - 1
+	}
+	if player.y < 0 {
+		player.y = 0
+	}
+	if player.y > height-1 {
+		player.y = height - 1
+	}
+
+	// for len(bf) != 0 {
+	// 	player.x += directionX
+	// 	player.y += directionY
+	// 	bf = blockFilter(block, func(b Block) bool {
+	// 		return b.x == player.x+directionX && b.y == player.y+directionY
+	// 	})
+	// }
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	board = [height][width]int{}
 	screen.Fill(background)
+
+	for _, blo := range block {
+		board[blo.y][blo.x] = 1
+	}
 
 	for y := -1; y < height+1; y++ {
 		for x := -1; x < width+1; x++ {
@@ -65,12 +131,29 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func getBlock(tile int) color.RGBA {
 	switch tile {
 	case 1:
-		return blue
+		return brown
 	case 2:
 		return green
 	default:
 		return blue
 	}
+}
+
+func blockFilter(v []Block, f func(b Block) bool) []Block {
+	var nb []Block
+	for _, bv := range v {
+		if f(bv) {
+			nb = append(nb, bv)
+		}
+	}
+
+	return nb
+}
+
+func init() {
+	block = append(block,
+		Block{Point{x: 9, y: 11}},
+		Block{Point{x: 0, y: 1}})
 }
 
 func main() {
